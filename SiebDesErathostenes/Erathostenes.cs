a3cf982e337a52d8e85f4a8fe3f05635559ebcf3
hannes leffers <hannesleffers@gmx.de>
2017-03-20 13:39:47 +0100

    Projektdateien hinzufügen.

---
+++ SiebDesErathostenes/Erathostenes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiebDesErathostenes
+{
+    class Erathostenes
+    {
+        private int i,j;
+        private bool[] prim;
+        public Erathostenes()
+        {
+
+        }
+
+        public void calculatePrim(int maxNumber)
+        {
+            Init(maxNumber);
+            while(i*i < prim.Length)
+            {
+
+            }
+        }
+        private void Init(int maxNumber)
+        {
+            i = 2;
+            prim = new bool[maxNumber];
+        }
+    }
+}
